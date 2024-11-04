0eb9a879d05033480ea21f17ed3aff2a28c88a44
Ondřej Urban <54501744+ondrau2@users.noreply.github.com>
2024-11-04 15:36:15 +0100

    Added histogram adjust controls

--- GUI_lib/TkinterGraphing.py
+++ GUI_lib/TkinterGraphing.py
@@ -4,10 +4,13 @@ from matplotlib.backends.backend_tkagg import (
 import tkinter as tk
 import customtkinter
 import numpy as np
+from CTkSpinbox import *
 
 ##Tkinter gui class for matplotlib
 class Tkinter_Graphing:
     def __init__(self, master):
+        self.xMax = customtkinter.IntVar(value  = 2000)
+        self.binAdjust = customtkinter.IntVar(value  = 1)
         self.fig, self.ax = plt.subplots()
         # Tkinter Application
         self.frame = tk.Frame(master)
@@ -16,6 +19,21 @@ class Tkinter_Graphing:
         # Create Canvas
         self.canvas = FigureCanvasTkAgg(self.fig, master)  
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.NONE, expand=0)#fill=tk.BOTH, expand=1)
+
+        #Spinbox
+        self.xlimitFrame = customtkinter.CTkFrame(master)
+        self.xlimitFrame.pack(side=tk.TOP, fill='none', expand=tk.TRUE)# fill='both', expand=1)
+        self.lbl_x_max = customtkinter.CTkLabel(self.xlimitFrame,text='X-limit: ')
+        self.lbl_x_max.pack(side=tk.LEFT, fill=tk.NONE, expand=True)
+        self.xMaxSpinbox = CTkSpinbox(self.xlimitFrame, start_value = 2000, min_value = 1, max_value = 2000,scroll_value = 50, step_value = 1, width = 300, variable = self.xMax)
+        self.xMaxSpinbox.pack(side=tk.LEFT, fill=tk.NONE, expand=True)
+
+        self.binFrame = customtkinter.CTkFrame(master)
+        self.binFrame.pack(side=tk.TOP, fill='none', expand=tk.TRUE)# fill='both', expand=1)
+        self.lbl_x_max = customtkinter.CTkLabel(self.binFrame,text='Bin adjust: ')
+        self.lbl_x_max.pack(side=tk.LEFT, fill=tk.NONE, expand=True)
+        self.xMaxSpinbox = CTkSpinbox(self.binFrame, start_value = 1, min_value = 1, max_value = 50,scroll_value = 1, step_value = 1, width = 300, variable = self.binAdjust)
+        self.xMaxSpinbox.pack(side=tk.LEFT, fill=tk.NONE, expand=True)
         
     #Add the colorbar
     def addColorBar(self, Im):
@@ -36,7 +54,24 @@ class Tkinter_Graphing:
         self.fig.clear()
         self.ax = self.fig.add_axes([0.1,0.1,0.8,0.8])
        
-        Im = self.ax.plot(x, y )
+        #Eventually adjust binning
+        if(self.binAdjust.get() != 1):
+            i = 0
+            x_i = x*self.binAdjust.get()
+            y_i = np.zeros(int(np.size(x)))
+            for value in y:
+                #assuming sorted array
+                if(value<self.binAdjust.get()*i):  
+                   y_i[i] = y_i[i] + value
+                   x_i[i] = i*self.binAdjust.get()
+                else:
+                   i =  i + 1
+
+            Im = self.ax.plot(x_i, y_i)
+        else: 
+            Im = self.ax.plot(x, y)
+
+        plt.xlim(0, self.xMax.get())
         
         self.canvas.draw()
 
@@ -46,7 +81,7 @@ class Tkinter_Graphing:
         self.fig.clear()
         self.ax = self.fig.add_axes([0.1,0.1,0.8,0.8])
         
-        Im = self.ax.hist(x, bins=2000, color='b')
+        Im = self.ax.hist(x, bins=2000, range = [0,500], color='b')
         
         self.canvas.draw()
 
@@ -59,3 +94,7 @@ class Tkinter_Graphing:
         Im = self.ax.bar(x, y )
         
         self.canvas.draw()
+
+    def updateLimits(self, lim):
+        self.plt.xlim(0, lim)
+    
